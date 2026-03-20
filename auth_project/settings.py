256d4697e49d238ff8fef5a16949605a8e0bff8b
Zibeon Jonriano Wisnumoerti <zibeonjonriano21@gmail.com>
2026-03-21 01:06:45 +0700

    creating frontend UI

--- auth_project/settings.py
+++ auth_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'landing',
 ]
 
 MIDDLEWARE = [
@@ -136,4 +137,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
